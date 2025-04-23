6dc737a7bb3405549946dd78701b9f5a7d5a276d
Fernando <fernando.nunes@presoft.com>
2025-04-23 20:13:53 -0300

    api com mongo e cache

---
+++ clima-api/config.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+# Substitua a string abaixo pela sua URI do MongoDB
+MONGO_URI = "mongodb://localhost:27017"
+DB_NAME = "clima_db"
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+clima_collection = db["clima"]
